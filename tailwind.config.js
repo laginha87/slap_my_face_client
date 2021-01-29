1fdb9b7ddb433256742ff18c68de1fb6f6728e06
Filipe Correia <filipe.lp.correia@gmail.com>
2021-01-29 20:17:08 +0000

    Adds purge css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
-  purge: [],
   darkMode: false, // or 'media' or 'class'
+  purge: [
+    './index.html',
+    './app/**/*.tsx',
+  ],
   theme: {
     fontFamily: {
       title: ["Potta One", "cursive"],
