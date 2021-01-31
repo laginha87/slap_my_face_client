9cd35448621609766b6b761ff63523fd47836a62
Filipe Correia <filipe.lp.correia@gmail.com>
2021-01-31 23:26:55 +0000

    And another try

--- deploy/deploy.rb
+++ deploy/deploy.rb
@@ -2,9 +2,5 @@
 `yarn build`
 
 Dir["dist/*.js"].map { |e| File.basename(e) }.each do |f|
-  puts `curl https://api.rollbar.com/api/1/sourcemap/download
-    -F access_token=1509ae28262b476abb18c0056b261f5b \
-    -F version=#{ENV["BUILD_ID"]} \
-    -F minified_url=https://slapmyface.net/#{f}`
-
+  puts `curl -X POST -H "Content-Type: application/json" -d '{"access_token":"1509ae28262b476abb18c0056b261f5b", "version": "#{ENV["BUILD_ID"]}", "minified_url":"https://slapmyface.net/#{f}"}' https://api.rollbar.com/api/1/sourcemap/download`
 end
