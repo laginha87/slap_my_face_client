e251ee282de48abaaf58d7f5588b1707c0daeb38
Filipe Correia <filipe.lp.correia@gmail.com>
2021-02-01 08:58:29 +0000

    AGAIN

--- deploy/deploy.rb
+++ deploy/deploy.rb
@@ -1,7 +1,7 @@
 
 `yarn build`
 
-`curl -X POST -H  -H "Content-Type: application/json" -d {"access_token":"1509ae28262b476abb18c0056b261f5b", "environment":"production", "revision":"#{ENV["BUILD_ID"]}"}' https://api.rollbar.com/api/1/deploy`
+`curl -X POST -H  -H "Content-Type: application/json" -d '{"access_token":"1509ae28262b476abb18c0056b261f5b", "environment":"production", "revision": "#{ENV["BUILD_ID"]}"}' https://api.rollbar.com/api/1/deploy`
 Dir["dist/*.js"].map { |e| File.basename(e) }.each do |f|
-  puts `curl -X POST -H "Content-Type: application/json" -d {"access_token":"1509ae28262b476abb18c0056b261f5b", "version": "#{ENV["BUILD_ID"]}", "minified_url":"https://slapmyface.net/#{f}"}' https://api.rollbar.com/api/1/sourcemap/download`
+  puts `curl -X POST -H "Content-Type: application/json" -d '{"access_token":"1509ae28262b476abb18c0056b261f5b", "version": "#{ENV["BUILD_ID"]}", "minified_url": "https://slapmyface.net/#{f}"}' https://api.rollbar.com/api/1/sourcemap/download`
 end
